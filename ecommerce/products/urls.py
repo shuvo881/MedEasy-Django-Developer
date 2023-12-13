cedf4ff7a9887e50b386c3319c2dff19c0e56efd
shuvo <golammostofa10001@gamil.com>
2023-12-13 16:46:40 +0600

    user and authectication completed

---
+++ ecommerce/products/urls.py
@@ -0,0 +1,12 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet#, ReviewViewSet, OrderViewSet
+
+router = DefaultRouter()
+router.register('products', ProductViewSet, basename='product')
+#router.register(r'reviews', ReviewViewSet, basename='review')
+#router.register(r'orders', OrderViewSet, basename='order')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
